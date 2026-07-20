187f5c3d394fd0a89439e577f2e99742de998f3a
Devanshi Aggarwal <devanshi.aggarwal06@gmail.com>
2026-07-20 23:38:58 +0530

    Create solution.py

---
+++ Day-47-Frog-2/solution.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+h = list(map(int, input().split()))
+
+INF = float('inf')
+dp = [INF] * N
+dp[0] = 0
+
+for i in range(1, N):
+    for j in range(max(0, i - K), i):
+        dp[i] = min(dp[i], dp[j] + abs(h[i] - h[j]))
+
+print(dp[-1])
